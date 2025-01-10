dfa7fdcb7a1071e2b49a6de6df83fb7abf5a6bf3
sandy <sandyknight90@gmail.com>
2025-01-10 15:57:09 +0000

    master fix #2

--- master-setup.sh
+++ master-setup.sh
@@ -1,28 +1,33 @@
 # Basics
-cd ~
-sudo apt-get -y update
-sudo apt-get -y upgrade
-sudo apt-get -y install build-essential
+apt update -qq && apt upgrade --yes
+apt install --yes build-essential
 
 # My shell scripts
 git clone git@github.com:sandyknight/shell-scripts.git
 
-
 # R
-sudo apt install -y r-base-dev
+apt install -y r-base
+
 Rscript -e 'install.packages("devtools")'
+
 Rscript -e 'install.packages("littler")'
+
 chmod +x shell-scripts/install-r-apt-repos.sh
-shell-scrupts/intstall-r-apt-repos.sh
+
+shell-scripts/intstall-r-apt-repos.sh
+
 install.r tidyverse data.table janitor tinythemes scales 
 
 # Editor
 sudo apt-get install -y emacs # Emacs
+
 git clone https://github.com/syl20bnr/spacemacs ~/.emacs.d # Spacemacs
 
 # Shell 
 sudo apt install -y zsh
+
 chsh -s $(which zsh) # Change default shell
+
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" # Install oh my zsh
 
 
@@ -32,5 +37,5 @@ cp -f ~/dotfiles/{.*} ~/
 mv aliases.zsh .oh-my-zsh/custom/aliases.zsh
 
 # Fonts
-sudo apt install -y fonts-firacode
+apt install -y fonts-firacode
 
